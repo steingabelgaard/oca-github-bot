379b14f31216cc4657dd2db6b63ada62a608d4f6
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-02-25 20:37:21 +0000

    Add log

--- src/oca_github_bot/tasks/update_pr_state.py
+++ src/oca_github_bot/tasks/update_pr_state.py
@@ -19,5 +19,6 @@ def update_pr_state(org, repo, pr, merged, dry_run=False):
             with odoo_client.login() as odoo:
                 Prs = odoo.env['project.git.pullrequest']
                 pr_ids = Prs.search([('pr_number', '=', pr_number), ('url', '=', gh_pr.html_url)])
+                _logger.info('PR: %d %s', pr_number, gh_pr.html_url)
                 for pr in Prs.browse(pr_ids):
                     pr.state = 'merged' if merged else 'closed'
