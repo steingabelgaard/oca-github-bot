e590c023108742ebfe5ece2fee78611a8049641f
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-02-25 20:17:50 +0000

    Typo

--- src/oca_github_bot/tasks/update_pr_state.py
+++ src/oca_github_bot/tasks/update_pr_state.py
@@ -15,7 +15,7 @@ def update_pr_state(org, repo, pr, merged, dry_run=False):
     with github.login() as gh:
         gh_pr = gh.pull_request(org, repo, pr)
         pr_number = gh_pr.number
-        if pr_number
+        if pr_number:
             with odoo_client.login() as odoo:
                 Prs = odoo.env['project.git.pullrequest']
                 pr_ids = Prs.search([('pr_number', '=', pr_number), ('url', '=', gh_pr.html_url)])
