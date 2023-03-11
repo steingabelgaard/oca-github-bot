7542e7a59cda95edbdd704bb5f5418852fc66d32
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-03-11 18:36:56 +0100

    Addons info to ogir

--- src/oca_github_bot/webhooks/on_push_to_main_branch.py
+++ src/oca_github_bot/webhooks/on_push_to_main_branch.py
@@ -5,6 +5,7 @@ import logging
 
 from ..router import router
 from ..tasks.main_branch_bot import main_branch_bot
+from ..tasks.add_modified_addons_to_ogir import add_modified_addons_to_ogir
 from ..version_branch import is_main_branch_bot_branch
 
 _logger = logging.getLogger(__name__)
@@ -19,6 +20,9 @@ async def on_push_to_main_branch(event, gh, *args, **kwargs):
     branch = event.data["ref"].split("/")[-1]
 
     if not is_main_branch_bot_branch(branch):
+        if "pull_request" in event.data:  # Push to PR
+            pr = event.data["pull_request"]["number"]
+            add_modified_addons_to_ogir.delay(org, repo, pr)
         return
 
     main_branch_bot.delay(org, repo, branch, build_wheels=False)
